002708f62ddb25afaad8e027dc8bdfa41c374311
richardsheridan <richard.sheridan@gmail.com>
2021-02-17 11:57:17 -0500

    misc string changes

--- trio_parallel/_worker_processes.py
+++ trio_parallel/_worker_processes.py
@@ -24,7 +24,7 @@ class BrokenWorkerError(RuntimeError):
     """Raised when a worker process fails or dies unexpectedly.
 
     This error is not typically encountered in normal use, and indicates a severe
-    failure of either Trio or the code that was executing in the worker.
+    failure of either trio-parallel or the code that was executing in the worker.
     """
 
     pass
