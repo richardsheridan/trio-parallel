c9e346ffad7967535c9f57303d0acd66cfbf94db
richardsheridan <richard.sheridan@gmail.com>
2021-02-21 10:25:01 -0500

    check race on mac

--- trio_parallel/_proc.py
+++ trio_parallel/_proc.py
@@ -158,6 +158,12 @@ class WindowsWorkerProc(WorkerProcBase):
 class PosixWorkerProc(WorkerProcBase):
     async def wait(self):
         await trio.lowlevel.wait_readable(self._proc.sentinel)
+        e = self._proc.exitcode
+        if e is not None:
+            return e
+        # race on macOS, see comment in trio.Process._wait
+        self._proc.join()
+        # unfortunately join does not return exitcode
         return self._proc.exitcode
 
     def _rehabilitate_pipes(self):
