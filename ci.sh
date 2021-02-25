c7064d120a494e267d77de70734e33853e16078e
richardsheridan <richard.sheridan@gmail.com>
2021-02-25 14:35:38 -0500

    remove source dir to ensure we are running from install dir

--- ci.sh
+++ ci.sh
@@ -6,36 +6,16 @@ set -ex -o pipefail
 uname -a
 env | sort
 
-# Curl's built-in retry system is not very robust; it gives up on lots of
-# network errors that we want to retry on. Wget might work better, but it's
-# not installed on azure pipelines's windows boxes. So... let's try some good
-# old-fashioned brute force. (This is also a convenient place to put options
-# we always want, like -f to tell curl to give an error if the server sends an
-# error response, and -L to follow redirects.)
-function curl-harder() {
-    for BACKOFF in 0 1 2 4 8 15 15 15 15; do
-        sleep $BACKOFF
-        if curl -fL --connect-timeout 5 "$@"; then
-            return 0
-        fi
-    done
-    return 1
-}
-
-################################################################
-# We have a Python environment!
-################################################################
-
-python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('#' * 70)"
+python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('#' * 70)"
 
 python -m pip install -U pip setuptools wheel
 python -m pip --version
 
 python setup.py sdist --formats=zip
 python -m pip install dist/*.zip
+rm -rf trio_parallel
 
 python -m pip install -r test-requirements.txt
-
 
 INSTALLDIR=$(python -c "import os, trio_parallel; print(os.path.dirname(trio_parallel.__file__))")
 
