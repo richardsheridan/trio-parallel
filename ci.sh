a82999287fc2ec235529859d4e1cdcdbd0a268c2
richardsheridan <richard.sheridan@gmail.com>
2021-02-25 12:16:51 -0500

    trim ci.sh back after making blacken job

--- ci.sh
+++ ci.sh
@@ -34,52 +34,33 @@ python -m pip --version
 python setup.py sdist --formats=zip
 python -m pip install dist/*.zip
 
-if python -c 'import sys; sys.exit(sys.version_info >= (3, 7))'; then
-    # Python < 3.7, select last ipython with 3.6 support
-    # macOS requires the suffix for --in-place or you get an undefined label error
-    sed -i'.bak' 's/ipython==[^ ]*/ipython==7.16.1/' test-requirements.txt
-    sed -i'.bak' 's/traitlets==[^ ]*/traitlets==4.3.3/' test-requirements.txt
-    git diff test-requirements.txt
-fi
+python -m pip install -r test-requirements.txt
 
-if [ "$CHECK_FORMATTING" = "1" ]; then
-    python -m pip install -r test-requirements.txt
-    false
+# We run the tests from inside an empty directory, to make sure Python
+# doesn't pick up any .py files from our working dir. Might have been
+# pre-created by some of the code above.
+mkdir empty || true
+cd empty
+
+INSTALLDIR=$(python -c "import os, trio_parallel; print(os.path.dirname(trio_parallel.__file__))")
+cp ../setup.cfg $INSTALLDIR
+# We have to copy .coveragerc into this directory, rather than passing
+# --cov-config=../.coveragerc to pytest, because codecov.sh will run
+# 'coverage xml' to generate the report that it uses, and that will only
+# apply the ignore patterns in the current directory's .coveragerc.
+cp ../pyproject.toml .
+if pytest -W error -r a --junitxml=../test-results.xml ${INSTALLDIR} --cov="$INSTALLDIR" --verbose; then
+    PASSED=true
 else
-    # Actual tests
-    python -m pip install -r test-requirements.txt
-
-    # So we can run the test for our apport/excepthook interaction working
-    if [ -e /etc/lsb-release ] && grep -q Ubuntu /etc/lsb-release; then
-        sudo apt install -q python3-apport
-    fi
-
-    # We run the tests from inside an empty directory, to make sure Python
-    # doesn't pick up any .py files from our working dir. Might have been
-    # pre-created by some of the code above.
-    mkdir empty || true
-    cd empty
-
-    INSTALLDIR=$(python -c "import os, trio_parallel; print(os.path.dirname(trio_parallel.__file__))")
-    cp ../setup.cfg $INSTALLDIR
-    # We have to copy .coveragerc into this directory, rather than passing
-    # --cov-config=../.coveragerc to pytest, because codecov.sh will run
-    # 'coverage xml' to generate the report that it uses, and that will only
-    # apply the ignore patterns in the current directory's .coveragerc.
-    cp ../pyproject.toml .
-    if pytest -W error -r a --junitxml=../test-results.xml ${INSTALLDIR} --cov="$INSTALLDIR" --verbose; then
-        PASSED=true
-    else
-        PASSED=false
-    fi
-
-    # The codecov docs recommend something like 'bash <(curl ...)' to pipe the
-    # script directly into bash as its being downloaded. But, the codecov
-    # server is flaky, so we instead save to a temp file with retries, and
-    # wait until we've successfully fetched the whole script before trying to
-    # run it.
-    curl-harder -o codecov.sh https://codecov.io/bash
-    bash codecov.sh -n "${JOB_NAME}"
-
-    $PASSED
+    PASSED=false
 fi
+
+# The codecov docs recommend something like 'bash <(curl ...)' to pipe the
+# script directly into bash as its being downloaded. But, the codecov
+# server is flaky, so we instead save to a temp file with retries, and
+# wait until we've successfully fetched the whole script before trying to
+# run it.
+curl-harder -o codecov.sh https://codecov.io/bash
+bash codecov.sh -n "${JOB_NAME}"
+
+$PASSED
