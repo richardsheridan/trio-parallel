be866919151e235cc076df435fa6d61abfac2db2
richardsheridan <richard.sheridan@gmail.com>
2021-02-26 08:13:22 -0500

    Use artifacts to make a separate build step
    also provides better guarantees of build and install isolation

--- ci.sh
+++ ci.sh
@@ -7,16 +7,12 @@ uname -a
 env | sort
 
 python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('#' * 70)"
-
-python -m pip install -U pip setuptools wheel
-python -m pip --version
-
-python setup.py sdist --formats=zip
-python -m pip install dist/*.zip
 rm -rf trio_parallel
+python -m pip install -U pip
+python -m pip install ./*.whl
 
 python -m pip install -r test-requirements.txt
 
 INSTALLDIR=$(python -c "import os, trio_parallel; print(os.path.dirname(trio_parallel.__file__))")
 
-pytest -W error -r a --cov-report xml "${INSTALLDIR}" --cov="$INSTALLDIR" --verbose
+pytest --pyargs trio_parallel._tests -W error -r a --cov-report xml "${INSTALLDIR}" --cov="$INSTALLDIR" --verbose
