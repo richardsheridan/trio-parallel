02f089ca75879eebe94fc343aabfc70c88cad781
richardsheridan <richard.sheridan@gmail.com>
2021-03-31 09:30:25 -0400

    versioning with setuptools_scm

--- trio_parallel/__init__.py
+++ trio_parallel/__init__.py
@@ -1,6 +1,10 @@
 """Top-level package for trio-parallel."""
 
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    import warnings
+    warnings.warn("trio_parallel._version module missing, continuing without __version__")
 
 from ._impl import (
     to_process_run_sync as run_sync,
