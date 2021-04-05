bb3c361ef5df7d43ab89944ac5330cd49ffa27a6
richardsheridan <richard.sheridan@gmail.com>
2021-04-05 07:56:50 -0400

    black

--- trio_parallel/__init__.py
+++ trio_parallel/__init__.py
@@ -7,5 +7,6 @@ from ._impl import (
 from ._util import BrokenWorkerError
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
