7343952574fcb45e454d90df5e56a2a951deda0c
richardsheridan <richard.sheridan@gmail.com>
2021-04-10 16:15:53 -0400

    revise run_sync error semantics
    run_sync is now responsible for reaping any zombies it creates

--- trio_parallel/_proc.py
+++ trio_parallel/_proc.py
@@ -89,6 +89,7 @@ class WorkerProcBase(abc.ABC):
             send_pipe.close()
 
     async def run_sync(self, sync_fn, *args) -> Optional[Outcome]:
+        result = None
         try:
             if not self._started.is_set():
                 await trio.to_thread.run_sync(self._proc.start)
@@ -97,20 +98,20 @@ class WorkerProcBase(abc.ABC):
                 self._child_recv_pipe.close()
                 self._started.set()
             await self._send(dumps((sync_fn, args), protocol=-1))
-            return loads(await self._recv())
+            result = loads(await self._recv())
         except trio.EndOfChannel:
             # Likely the worker died while we were waiting on _recv
-            self.kill()  # NOTE: must reap zombie child elsewhere
             raise BrokenWorkerError(f"{self._proc} died unexpectedly")
         except trio.BrokenResourceError:
             # Likely the worker died while we were waiting on _send
-            self.kill()  # NOTE: must reap zombie child elsewhere
             return None
-        except BaseException:
-            # Cancellation or other unknown errors leave the process in an
-            # unknown state, so there is no choice but to kill.
-            self.kill()  # NOTE: must reap zombie child elsewhere
-            raise
+        finally:
+            if result is None:
+                self.kill()
+                with trio.CancelScope(shield=True):
+                    await self.wait()
+            else:
+                return result
 
     def is_alive(self):
         # if the proc is alive, there is a race condition where it could be
