bdd47e3e757a49fed1f83ca5cf4578c6609df3d9
richardsheridan <richard.sheridan@gmail.com>
2021-09-04 19:27:29 -0400

    update example

--- docs/source/examples/single_use_workers.py
+++ docs/source/examples/single_use_workers.py
@@ -9,16 +9,20 @@ def worker(i):
     print(i, "hello from", os.getpid())
 
 
+def after_single_use():
+    return True
+
+
 WORKER_HAS_BEEN_USED = False
 
 
-def after_single_use():
+def after_dual_use():
     global WORKER_HAS_BEEN_USED
     if WORKER_HAS_BEEN_USED:
         return True  # retire
     else:
         WORKER_HAS_BEEN_USED = True
-        return False  # don't retire
+        return False  # don't retire... YET
 
 
 async def amain():
@@ -26,6 +30,12 @@ async def amain():
 
     print("single use worker behavior:")
     with trio_parallel.cache_scope(retire=after_single_use):
+        async with trio.open_nursery() as nursery:
+            for i in range(40):
+                nursery.start_soon(trio_parallel.run_sync, worker, i)
+
+    print("dual use worker behavior:")
+    with trio_parallel.cache_scope(retire=after_dual_use):
         async with trio.open_nursery() as nursery:
             for i in range(40):
                 nursery.start_soon(trio_parallel.run_sync, worker, i)
