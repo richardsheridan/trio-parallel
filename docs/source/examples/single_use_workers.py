b533369ff2faf7d1de583dc81eb949001267309e
richardsheridan <richard.sheridan@gmail.com>
2021-09-04 19:41:30 -0400

    correct example

--- docs/source/examples/single_use_workers.py
+++ docs/source/examples/single_use_workers.py
@@ -29,13 +29,13 @@ async def amain():
     trio_parallel.current_default_worker_limiter().total_tokens = 4
 
     print("single use worker behavior:")
-    with trio_parallel.cache_scope(retire=after_single_use):
+    async with trio_parallel.cache_scope(retire=after_single_use):
         async with trio.open_nursery() as nursery:
             for i in range(40):
                 nursery.start_soon(trio_parallel.run_sync, worker, i)
 
     print("dual use worker behavior:")
-    with trio_parallel.cache_scope(retire=after_dual_use):
+    async with trio_parallel.cache_scope(retire=after_dual_use):
         async with trio.open_nursery() as nursery:
             for i in range(40):
                 nursery.start_soon(trio_parallel.run_sync, worker, i)
